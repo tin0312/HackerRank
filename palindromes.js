655781d39a0c96cce7e39f0c2509917ec379bc53
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-10-10 18:10:09 -0400

    palindromes function

---
+++ palindromes.js
@@ -0,0 +1,23 @@
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
+
+*/
+
+function isPalindrome(str){
+    // split the word to an array of letters
+   let splittedwWord = str.split("")
+   // reverse the letters 
+   let reversewords = splittedwWord.reverse().join("")
+        return str === reversewords
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
