871c8e74c2cb4d30059dc5c61a1c33562871d7c7
tin0312 <105316547+tin0312@users.noreply.github.com>
2023-10-16 00:10:03 -0400

    Conditionally rendering podcast objects

---
+++ conditionalObjRender.js
@@ -0,0 +1,113 @@
+/* 🌴 Save the Weekend 🌴
+
+Your best friend is a copywriter who writes product descriptions 
+for a living. You want to use your hacking skills to help them 
+automate their job so you both can spend the weekend on a 
+tropical island. 
+
+Use array methods and the existing podcast data to write a function that
+can generate a description for each podcast. 
+
+Add the description as a new property on each podcast object, and return
+a new podcast array where each podcast has a description. 
+
+Each description should look like this: 
+[
+    {
+        id: 1,
+        title: "Scrimba Podcast", 
+        ...
+        description: "Scrimba Podcast is a 50 minute education podcast hosted 
+        by Alex Booker."
+    }
+    ...
+]
+
+If the podcast has more than one host, you can display only the first host.
+
+Stretch goal: Display all three hosts in the description, seperated with commas: 
+
+Example description: "Coding Corner is a 55 minute education podcast hosted by Treasure Porth, Guil Hernandez, and Tom Chant."
+*/
+
+const podcasts = [
+  {
+    id: 1,
+    title: "Scrimba Podcast",
+    duration: 50,
+    tags: ["education", "jobs", "technology"],
+    hosts: ["Alex Booker"],
+    rating: 10,
+    genre: "education",
+    paid: false,
+  },
+  {
+    id: 2,
+    title: "Crime Fan",
+    duration: 150,
+    tags: ["crime", "entertainment", "mature"],
+    hosts: ["Bob Smith", "Camilla Lambert"],
+    genre: "true crime",
+    rating: 5,
+    paid: true,
+  },
+  {
+    id: 3,
+    title: "Mythical Creatures",
+    duration: 99,
+    tags: ["entertainment", "general", "unicorns"],
+    hosts: ["Esmerelda Shelley", "Duke Dukington", "Felix the Cat"],
+    genre: "fantasy",
+    rating: 8,
+    paid: true,
+  },
+  {
+    id: 4,
+    title: "Crime Crime Crime",
+    duration: 70,
+    tags: ["crime", "entertainment", "mature"],
+    hosts: ["Jessica Jones", "Humphrey Bogart", "Inspector Gadget"],
+    genre: "true crime",
+    rating: 6,
+    paid: true,
+  },
+  {
+    id: 5,
+    title: "Something about Witches",
+    duration: 35,
+    tags: ["fantasy", "entertainment"],
+    hosts: ["Frewin Wyrm", "Evanora Highmore"],
+    genre: "fantasy",
+    rating: 8,
+    paid: false,
+  },
+  {
+    id: 6,
+    title: "Coding Corner",
+    duration: 55,
+    tags: ["education", "jobs", "technology"],
+    hosts: ["Treasure Porth", "Guil Hernandez", "Tom Chant"],
+    genre: "education",
+    rating: 9,
+    paid: false,
+  },
+];
+function createDescriptionsFor(data) {
+  // loop through the podcast data
+  let descriptivePodcasts = data.map((podcast) => {
+    // check if there are more than one host
+    let allHosts = podcast.hosts.length > 1 ? podcast.hosts.join(", ") : podcast.hosts[0];
+    
+    return {
+      id: podcast.id,
+      title: podcast.title,
+      description: `${podcast.title} is a ${podcast.duration} minute ${podcast.tags[0]} podcast hosted by ${allHosts}.`,
+    };
+  });
+  return descriptivePodcasts;
+}
+
+console.log(createDescriptionsFor(podcasts));
+
+
+console.log(createDescriptionsFor(podcasts));
